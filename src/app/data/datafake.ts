27f3391f7e7cc0424cd6950ed5d20dc813c6b2fb
jsantosdev <jp.santos1454@gmail.com>
2023-10-14 02:03:15 -0300

    content created

---
+++ src/app/data/datafake.ts
@@ -0,0 +1,32 @@
+export const dataFake = [
+
+  {
+    "id": "1",
+
+    "photoCover": "https://i.ytimg.com/vi/D1KyqSG6UBY/maxresdefault.jpg",
+    "title": "Novo forza trás vida de volta à franquia",
+    "description": "",
+    "contentText": "Lançado na ultima semana, o jogo mostra que a Microsoft ainda consegue fazer excelentes jogos de corrida"
+  },
+  {
+    "id": "2",
+    "photoCover": "https://cdn.motor1.com/images/mgl/y2PRoY/s3/2024-peugeot-208-facelift.jpg",
+    "title": "Novo Peugeot 208 2024 estreia design mais agressivo e cabine conectada",
+    "description": "",
+    "contentText": ""
+  },
+  {
+    "id": "3",
+    "photoCover": "https://s2-ge.glbimg.com/PjHRC5xkEVLhwLFpyaLx_1HSuYc=/0x0:4549x3033/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2023/i/Z/xShoVyRHqLD9On2Fq9Eg/gettyimages-1726383545.jpg",
+    "title": "GP do Qatar é palco de nova polêmica",
+    "description": "",
+    "contentText": ""
+  },
+  {
+    "id": "4",
+    "photoCover": "https://www.otempo.com.br/image/contentid/policy:1.3253868:1697224870/Piloto-de-Formula-1-Lance-Stroll-jpg.jpg?f=3x2&w=1224",
+    "title": "Lance Stroll se desculpa por conduta no GP do Qatar e é punido",
+    "description": "",
+    "contentText": ""
+  },
+]
---
+++ src/app/pages/content/content.component.ts
@@ -0,0 +1,40 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { __values } from 'tslib';
+import { dataFake } from '../../data/datafake';
+
+@Component({
+  selector: 'app-content',
+  templateUrl: './content.component.html',
+  styleUrls: ['./content.component.css'],
+})
+export class ContentComponent implements OnInit {
+  photoCover:string = ""
+  contentTitle:string = ""
+  contentDescription:string = ""
+  contentText:string = ""
+  private id:string | null = "0"
+
+  constructor(
+    private route:ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe( value =>
+     this.id = value.get("id")
+    )
+
+    this.setValuesToComponent(this.id)
+  }
+
+  setValuesToComponent(id:string | null){
+    const result = dataFake.filter(article => article.id == id)[0]
+
+    this.contentTitle = result.title
+    this.contentDescription = result.description
+    this.photoCover = result.photoCover
+    this.contentText = result.contentText
+  }
+
+
+}
